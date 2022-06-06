4de15bff15041dc2cbc7e5ceb4bbc341296d4c19
Aiman <aiman.ahmedpro@gmail.com>
2022-06-06 15:08:19 +0200

    Ajout affichage recettes/debut tags

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const recipesSection = document.getElementById("recipes")
+populateRecipes(recipes)
+
+function populateRecipes(recipes){
+    recipesLength = recipes.length || recipes.size
+    for (let i = 0 ; i< recipesLength ; i++ ){
+        const recipe = Array.from(recipes)[i]
+        showRecipe(recipe)
+    }
+}
+
+
+function showRecipe(recipe) {
+    const elt = document.getElementById('receipt-model');
+    const dupNode = document.importNode(elt.content,true);
+    dupNode.querySelector('.receipt-name').textContent= recipe.name;
+    dupNode.querySelector('.receipt-duration').textContent= recipe.time;
+    const ingredientsList = dupNode.querySelector(".receipt-ingredients")
+    const ingredientModel = document.getElementById('ingredient-model');
+    for (let i = 0 ; i < recipe.ingredients.length ; i++){
+        const ingredient = recipe.ingredients[i]
+        const dupIngredientNode = document.importNode(ingredientModel.content,true);
+        dupIngredientNode.querySelector('.receipt-ingredient-name').textContent = ingredient.ingredient
+        if (ingredient.quantity) {
+            dupIngredientNode.querySelector('.receipt-ingredient-quantity').textContent = ': ' + ingredient.quantity
+        }
+        if (ingredient.unit) {
+            dupIngredientNode.querySelector('.receipt-ingredient-unit').textContent = ingredient.unit
+        }
+        ingredientsList.appendChild(dupIngredientNode)
+    }
+    dupNode.querySelector('.receipt-instructions').textContent = recipe.description
+    recipesSection.appendChild(dupNode);
+}
+
---
+++ scripts/tags.js
@@ -0,0 +1,46 @@
+const tagsButtons = document.querySelectorAll('.tags-btn')
+const tagsNameArray = ['ustensils', 'appliances', 'ingredients']
+const closeTagButtons = document.querySelectorAll('.tags-opened i')
+
+
+
+
+
+for (let i = 0 ; i < closeTagButtons.length ; i++) {
+    const closeTagButton = closeTagButtons[i]
+    closeTagButton.addEventListener('click', function(){
+        closeTag(closeTagButton.className.split(' ')[0].split('-')[1])
+    })
+}
+for (let i = 0 ; i < tagsButtons.length ; i++){
+    const tagsButton = tagsButtons[i]
+    tagsButton.addEventListener('click' , function() {
+        openTag(tagsButton.className.split('-')[0])
+    })
+}
+
+const tagsItems = {
+    ingredientsTagsItems: document.querySelector('.ingredients-opened .tags-items'),
+    ustensilsTagsItems: document.querySelector('.ustensils-opened .tags-items'),
+    appliancesTagsItems: document.querySelector('.appliances-opened .tags-items')
+}
+function openTag(tag){
+    for (let i = 0 ; i < tagsNameArray.length ; i++ ){
+        const tagName = tagsNameArray[i]
+        if (tagName !== tag) closeTag(tagName)
+    }
+    const tagToOpen = document.querySelector('.'+ tag + '-opened' )
+    const buttonToHide = document.querySelector('.'+ tag + '-btn' )
+    tagToOpen.style.display = 'block'
+    tagToOpen.querySelector('input').focus()
+    buttonToHide.style.setProperty('display','none', 'important')
+}
+
+function closeTag(tag){
+    const tagToHide = document.querySelector('.'+ tag + '-opened' )
+    const buttonToShow = document.querySelector('.'+ tag + '-btn' )
+    tagToHide.style.display = 'none'
+    buttonToShow.style.setProperty('display','block', 'important')
+    tagsInput[tag].value = ''
+    hydateTagByText(tag)
+}
